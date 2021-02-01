a1e36c9c11699ee054d59d5df7c697f5b99619f1
Josh <JMWilliams3790@gmail.com>
2021-01-31 17:37:07 -0800

    added two more layers, and some of th ecode for the boundaries

--- static/js/logic.js
+++ static/js/logic.js
@@ -17,18 +17,41 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // Use this link to get the geojson data.
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 
+var plateStyle = {
+  color: "orange",
+  weight: 1.5
+};
+
+var plates = d3.json("static/data/plates.json").then(function(data){
+  console.log(data)
+  L.geoJson(data, { 
+    onEachFeature: function(feature, layer) {
+        // Set mouse events to change map styling
+        layer.on({
+        
+          // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+          click: function(event) {
+            myMap.fitBounds(event.target.getBounds());
+          }
+        });
+      }
+    }
+  )}
+);
+
+
 // Function that will determine the color of a neighborhood based on the borough it belongs to
-function chooseColor(mag) {
+function chooseColor(value) {
   switch (true) {
-  case mag > 6.9:
+  case value > 90:
     return "red";
-  case mag > 6.4:
+  case value > 69:
     return "orangered";
-  case mag > 5.9:
+  case value > 49:
     return "orange";
-  case mag > 5.4:
+  case value > 29:
     return "yellow";
-  case mag > 4.9:
+  case value > 9:
     return "yellowgreen";
   default:
     return "green";
@@ -36,7 +59,7 @@ function chooseColor(mag) {
 }
 
 // Grabbing our GeoJSON data..
-d3.json(link).then(function(data) {
+var quakeMarker = d3.json(link).then(function(data) {
   // Creating a geoJSON layer with the retrieved data
   L.geoJson(data, {
     // Style each feature (in this case a neighborhood)
@@ -57,7 +80,7 @@ d3.json(link).then(function(data) {
       return {
         color: "black",
         // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-        fillColor: chooseColor(feature.properties.mag),
+        fillColor: chooseColor(feature.geometry.coordinates[2]),
         fillOpacity: 0.5,
         radius: feature.properties.mag * 4,
         weight: 1.5
@@ -65,12 +88,42 @@ d3.json(link).then(function(data) {
     }
   }).addTo(myMap);
 
-  var legend = L.control({position: 'bottomright'});
+  var quakeLayer = L.layerGroup(quakeMarker);
+
+  // Define variables for our tile layers
+  var light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
+  
+  var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+  
+  // Only one base layer can be shown at a time
+  var baseMaps = {
+    Light: light,
+    Dark: dark,
+    Street: myMap
+  };
+  
+  // Overlays that may be toggled on or off
+  var overlayMaps = {
+    Quakes: quakeLayer
+  };
+  L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+var legend = L.control({position: 'bottomright'});
 
 legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [4.5, 4.9, 5.4, 5.9, 6.4, 6.9],
+        grades = [-10, 10, 30, 50, 70, 90],
         labels = [];
         colors = ["green", "yellowgreen", "yellow", "orange", "orangered", "red"];
 
@@ -82,8 +135,12 @@ legend.onAdd = function (map) {
     }
 
     return div;
+
+
 };
+
 
 legend.addTo(myMap);
 
 });
+
